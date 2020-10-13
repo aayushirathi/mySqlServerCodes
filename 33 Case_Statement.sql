a0a1a8635777d0d8768e5b1734d219af3fa28e09
aayushirathi <71968675+aayushirathi@users.noreply.github.com>
2020-10-13 23:23:04 +0530

    Add files via upload

---
+++ 33 Case_Statement.sql
@@ -0,0 +1,34 @@
+Link- https://rextester.com/JOQPQN18990
+
+
+CREATE TABLE Cars
+(
+    id INT,
+    name VARCHAR(50) NOT NULL,
+    company VARCHAR(50) NOT NULL,
+    power INT NOT NULL,
+    color VARCHAR(50) NOT NULL,
+    model INT NOT NULL,
+    condition VARCHAR(50) NOT NULL
+ )
+
+INSERT INTO Cars
+VALUES
+(1, 'Corrolla', 'Toyota', 1800, 'red', 1995, 'X'),
+(2, 'City', 'Honda', 1500 , 'black', 2015, 'X'),
+(3, 'C200', 'Mercedez', 2000 , 'white', 1992, 'X'),
+(4, 'Vitz', 'Toyota', 1300 , 'blue', 2007, 'X'),
+(5, 'Baleno', 'Suzuki', 1500 , 'white', 2012, 'X'),
+(6, 'C500', 'Mercedez', 5000 , 'grey', 1994, 'X'),
+(7, '800', 'BMW', 8000 , 'blue', 2016, 'X'),
+(8, 'Mustang', 'Ford', 5000 , 'red', 1997, 'X'),
+(9, '208', 'Peugeot', 5400, 'black', 1999, 'X'),
+(10, 'Prius', 'Toyota', 3200 , 'red', 2003, 'X') ;
+
+SELECT * FROM Cars;
+
+SELECT name, model,
+CASE WHEN model > 2000 THEN 'New' 
+ELSE 'Old' END AS condition
+FROM Cars
+
